5fd8513ec9ed8c740b243c26fdc37217e20395dc
Dmitrii Dolbik <dda1508@gmail.com>
2022-12-26 17:36:50 +0300

    add solution

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -1,6 +1,16 @@
 package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
+    public static void main(String[] args) {
+        LeapYearPrinter leapYearPrinter = new LeapYearPrinter();
+        leapYearPrinter.isLeapYear(2000);
+    }
+
     public void isLeapYear(int year) {
+               if (year > 1584 && (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))) {
+            System.out.println("leap");
+        } else{
+            System.out.println("not leap");
+        }
     }
 }
